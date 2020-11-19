78c14dcab58f20a45c0433929822a774574e3322
jayme.weiss <jayme.weiss@entelect.co.za>
2020-11-19 09:24:43 +0200

    add custom observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private obsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    // this.obsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+    const customObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.obsSubscription = customObservable.subscribe((data) => {
+        console.log(data);
+      },
+      (error) => {
+        alert(error.message);
+      },
+      () => {
+        console.log('completed');
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.obsSubscription.unsubscribe();
   }
 
 }
